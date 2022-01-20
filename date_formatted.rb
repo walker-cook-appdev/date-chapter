a6a77ff2203b24003b84253835aa9cc9876d3e06
walker-cook <jwcook99@gmail.com>
2022-01-20 03:33:23 +0000

    Done

--- date_formatted.rb
+++ date_formatted.rb
@@ -1,3 +1,9 @@
 # Should format and identify different parts of todays date.
 # 
 # "The year is: 2020, the calendar day is: 1, and the month is: 7."
+
+require "date"
+
+today = Date.today
+
+p "The year is: " + today.year.to_s + ", the calendar day is: " + today.day.to_s + ", and the month is: " + today.month.to_s + "."
--- date_math.rb
+++ date_math.rb
@@ -5,3 +5,11 @@
 # Output:
 #   "Ruby is 108937 days old!"
 #
+
+require "date"
+
+
+
+num_days = Date.today - Date.parse("December 21, 1995")
+
+p "Ruby is " + num_days.to_i.to_s + " days old!"
